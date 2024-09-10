da64b26e781c732ae82fe5262b42e2e4b9d88fd6
MrEriikR <ek22_07@hotmail.com>
2024-09-09 22:50:19 -0600

    Animaciones en la main page

--- js/main.js
+++ js/main.js
@@ -13,10 +13,11 @@ window.addEventListener("load", scrollOnClick);
 
 // INTRO ANIMATIONS ---------------------------------------------------------
 
-// var introAnim = gsap.timeline({paused:true, delay: 1})
-// introAnim
-// .from('.qro', 1, { autoAlpha: 0, scale: 0.9})
-// window.addEventListener("load", introAnim.play());
+var introAnim = gsap.timeline({paused:true, delay: 0.5})
+introAnim
+.from('.section-heading, .subtitle', 1, { autoAlpha: 0, scale: 0.9, stagger: 0.1}, 'all')
+.from('.demo', 1, { autoAlpha: 0, scale: 0.9, ease: 'back.out'}, 'all')
+window.addEventListener("load", introAnim.play());
 
 
 //  MOBILE MENU -------------------------------------------------------------
