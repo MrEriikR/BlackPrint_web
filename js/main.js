2b486d6dba820972e8924a0288befe4f6389b2eb
MrEriikR <ek22_07@hotmail.com>
2024-09-09 23:06:42 -0600

    Se ajusto el timing de las animaciones

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 
 
 // LOADING SCREEN -----------------------------------------------------
+const loadingScreen = document.getElementById('loading-screen');
 window.addEventListener('load', function() {
-    var loadingScreen = document.getElementById('loading-screen');
-    loadingScreen.classList.add('hide'); // Hide the loading screen
+    loadingScreen.classList.add('hide');
+    if (loadingScreen.classList.contains("hide")) {
+        introAnim.play();
+    }
 });
 
 
@@ -17,7 +20,7 @@ var introAnim = gsap.timeline({paused:true, delay: 0.5})
 introAnim
 .from('.section-heading, .subtitle', 1, { autoAlpha: 0, scale: 0.9, stagger: 0.1}, 'all')
 .from('.demo', 1, { autoAlpha: 0, scale: 0.9, ease: 'back.out'}, 'all')
-window.addEventListener("load", introAnim.play());
+
 
 
 //  MOBILE MENU -------------------------------------------------------------
