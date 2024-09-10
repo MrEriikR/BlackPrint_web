72bb341ac03120a0ce356a21d3da4b202e21d781
MrEriikR <ek22_07@hotmail.com>
2024-09-10 12:19:15 -0600

    Video Background agregado

--- js/main.js
+++ js/main.js
@@ -18,8 +18,8 @@ window.addEventListener("load", scrollOnClick);
 
 var introAnim = gsap.timeline({paused:true, delay: 0.5})
 introAnim
-.from('.section-heading, .subtitle', 1, { autoAlpha: 0, scale: 0.9, stagger: 0.1}, 'all')
-.from('.demo', 1, { autoAlpha: 0, scale: 0.9, ease: 'back.out'}, 'all')
+.from('.section-heading, .subtitle', 2, { autoAlpha: 0, scale: 0.9, stagger: 0.1}, 'all')
+.from('.demo', 2, { autoAlpha: 0, scale: 0.9}, 'all')
 
 
 
